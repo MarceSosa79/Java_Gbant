b741e67eb9f3eacdf531c266d7ceb1502240a2e9
Marcelo Sosa <marcelososat79@gmail.com>
2024-12-04 20:37:16 -0300

    Inicio en Java

---
+++ Introduccion_Java/MiPrimerPrograma.java
@@ -0,0 +1,39 @@
+package Introduccion_Java;
+
+public class MiPrimerPrograma {
+    public static void main(String[] args) throws Exception {
+        //Asignacion de variables
+        /*int miEntero;
+        miEntero = 2;
+        int edad = 28;*/
+        //System.out.println("Mi edad es: " + edad);
+        //Solo Print line
+        /*System.out.println("Hola Mundo...");
+        System.out.println("Bienvenido a tu curso de Java");
+        System.out.println("Mi nombre es Marcelo");
+        System.out.println("Mi edad es 45");*/
+        //Booleano
+        boolean miBool;
+        miBool = true;
+        //Char
+        char miChar;
+        miChar = 'a';
+        //String
+        String miString;
+        miString = "Vamos al ";
+        //Int
+        int miInt;
+        miInt = 10;
+        //Float
+        float miFloat;
+        miFloat = 1.75f;
+        //Long
+        long miLong;
+        miLong = 123456789; 
+        System.out.println(miString + miFloat + " " + miLong);
+        int edad = 45;
+        String nombre = "Marcelo";
+        System.out.println("Mi nombre es " + nombre + ", y tengo " + edad + " años.");
+        
+    }
+}
