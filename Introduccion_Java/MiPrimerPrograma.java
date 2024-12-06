7ea057b5fdba365496e84b0be03dcc1a8127ac89
Marcelo Sosa <marcelososat79@gmail.com>
2024-12-06 19:59:30 -0300

    Agregados

--- Introduccion_Java/MiPrimerPrograma.java
+++ Introduccion_Java/MiPrimerPrograma.java
@@ -1,54 +1,66 @@
 package Introduccion_Java;
+
 import java.util.Scanner;
 
 public class MiPrimerPrograma {
     @SuppressWarnings("unused")
     public static void main(String[] args) throws Exception {
-        //Asignacion de variables
-        /*int miEntero;
-        miEntero = 2;
-        int edad = 28;*/
-        //System.out.println("Mi edad es: " + edad);
-        //Solo Print line
-        /*System.out.println("Hola Mundo...");
-        System.out.println("Bienvenido a tu curso de Java");
-        System.out.println("Mi nombre es Marcelo");
-        System.out.println("Mi edad es 45");*/
-        //Booleano
-        /*boolean miBool = true;
-        //Char
-        char miChar = 'a';
-        //String
-        String miString = "Vamos al ";
-        //Int
-        int miInt = 10;
-        //Float
-        float miFloat = 1.75f;
-        //Long
-        long miLong = 123456789; 
-        System.out.println(miString + miFloat + " " + miLong);
-        //Declarar y mostrar variables
-        int edad = 45;
-        String nombre = "Marcelo";
-        System.out.println("Mi nombre es " + nombre + ", y tengo " + edad + " años.");
-        //Ultima parte
-        String mensajeBienvenida = "Bienvenido a Java ";
-        double temperaturaActual = 19;
-        boolean datoLogico = true;
-        System.out.println("Hola, " + mensajeBienvenida +", la temperatura actual es de " + temperaturaActual +" grados. Esta afirmacion es verdadera " + datoLogico);*/
+        logic();
+        // Asignacion de variables
+        /*
+         * int miEntero;
+         * miEntero = 2;
+         * int edad = 28;
+         */
+        // System.out.println("Mi edad es: " + edad);
+        // Solo Print line
+        /*
+         * System.out.println("Hola Mundo...");
+         * System.out.println("Bienvenido a tu curso de Java");
+         * System.out.println("Mi nombre es Marcelo");
+         * System.out.println("Mi edad es 45");
+         */
+        // Booleano
+        /*
+         * boolean miBool = true;
+         * //Char
+         * char miChar = 'a';
+         * //String
+         * String miString = "Vamos al ";
+         * //Int
+         * int miInt = 10;
+         * //Float
+         * float miFloat = 1.75f;
+         * //Long
+         * long miLong = 123456789;
+         * System.out.println(miString + miFloat + " " + miLong);
+         * //Declarar y mostrar variables
+         * int edad = 45;
+         * String nombre = "Marcelo";
+         * System.out.println("Mi nombre es " + nombre + ", y tengo " + edad +
+         * " años.");
+         * //Ultima parte
+         * String mensajeBienvenida = "Bienvenido a Java ";
+         * double temperaturaActual = 19;
+         * boolean datoLogico = true;
+         * System.out.println("Hola, " + mensajeBienvenida
+         * +", la temperatura actual es de " + temperaturaActual
+         * +" grados. Esta afirmacion es verdadera " + datoLogico);
+         */
 
-/* 
-//CLase 2
-        Scanner pepe = new Scanner(System.in);
-        System.out.print("Por favor ingrese una edad: ");
-        int edad = pepe.nextInt();
-        pepe.nextLine();
-        System.out.print("Por favor ingrese un nombre completo: ");
-        String nombre = pepe.nextLine();
-        System.out.println(nombre + " tiene " + edad + " años.");
-        */
-    //Declare 2 variables y asignales un valor a cada una de ellas.
+        /*
+         * //CLase 2
+         * Scanner pepe = new Scanner(System.in);
+         * System.out.print("Por favor ingrese una edad: ");
+         * int edad = pepe.nextInt();
+         * pepe.nextLine();
+         * System.out.print("Por favor ingrese un nombre completo: ");
+         * String nombre = pepe.nextLine();
+         * System.out.println(nombre + " tiene " + edad + " años.");
+         */
+        // Declare 2 variables y asignales un valor a cada una de ellas.
 
+        @SuppressWarnings("resource")
         Scanner variables = new Scanner(System.in);
         System.out.println("Ingrese la primera variable: ");
         int num1 = variables.nextInt();
@@ -60,12 +72,27 @@ public class MiPrimerPrograma {
         double division = num1 / num2;
         int multiplicar = num1 * num2;
         int modulo = num1 % num2;
-       
+
         System.out.println("La suma de las 2 variables es = a: " + suma);
         System.out.println("La resta de las 2 variables es = a: " + resta);
         System.out.println("La division de las 2 variables es = a: " + division);
         System.out.println("La multiplicacion de las 2 variables es = a: " + multiplicar);
         System.out.println("El modulo de las 2 variables es: " + modulo);
+    }
+
+    // 1. Pide al usuario que ingrese dos números, y:
+    // A. Compara si el primer número ingresado es mayor al segundo número
+    // ingresado.
+    public static void logic() {
+        Scanner nuevo = new Scanner(System.in);
+        System.out.println("Dame un numero entero");
+        int num1 = nuevo.nextInt();
+        System.out.println("Dame un segundo numero entero");
+        int num2 = nuevo.nextInt();
+
+        System.out.println("El numero " + num1 + " es mayor a " + num2 + " = " + (num1 > num2));
+        System.out.println("El numero " + num1 + " es distinto a " + num2 + " = " + (num1 != num2));
+        System.out.println("El numero " + num1 + " es divisible entre 2 = " + (num1 % 2 == 0));
 
     }
 }
---
+++ VerificacionEdad.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class VerificacionEdad {
+    public static void main(String[] args) {
+        // Declarar el año actual como constante
+        final int ANIOACTUAL = 2024;
+
+        // Crear un objeto Scanner para entrada de datos
+        Scanner scanner = new Scanner(System.in);
+
+        // Pedir al usuario su año de nacimiento
+        System.out.print("Por favor, ingresa tu año de nacimiento: ");
+        int anioNacimiento = scanner.nextInt();
+
+        // Calcular la edad
+        int calculoEdad = ANIOACTUAL - anioNacimiento;
+
+        // Determinar el mensaje directamente usando operador ternario
+        String mensaje = calculoEdad >= 18
+                ? "Tienes " + calculoEdad + " años. Eres mayor de edad."
+                : "Tienes " + calculoEdad + " años. Eres menor de edad.";
+
+        // Mostrar el mensaje
+        System.out.println(mensaje);
+
+        scanner.close();
+    }
+}
