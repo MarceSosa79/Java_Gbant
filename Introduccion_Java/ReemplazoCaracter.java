0415d9c6c0083c95d0fca8d2e70b321455eba650
Marcelo Sosa <marcelososat79@gmail.com>
2024-12-17 19:02:40 -0300

    .

--- Introduccion_Java/ReemplazoCaracter.java
+++ Introduccion_Java/ReemplazoCaracter.java
@@ -1,22 +1,21 @@
 import java.util.Scanner;
 
 public class ReemplazoCaracter {
-public static void main(String[] args) {
-try (Scanner miScanner = new Scanner(System.in)) {
-System.out.println("Ingresa una frase: ");
-String frase = miScanner.nextLine();
-System.out.println("Ingresa caracter 1: ");
-char caracter1 = miScanner.next().charAt(0);
-System.out.println("Ingresa caracter 2: ");
-char caracter2 = miScanner.next().charAt(0);
+    public static void main(String[] args) {
+        try (Scanner miScanner = new Scanner(System.in)) {
+            System.out.println("Ingresa una frase: ");
+            String frase = miScanner.nextLine();
+            System.out.println("Ingresa caracter 1: ");
+            char caracter1 = miScanner.next().charAt(0);
+            System.out.println("Ingresa caracter 2: ");
+            char caracter2 = miScanner.next().charAt(0);
 
-String resultado = frase.replace(caracter1, caracter2);
+            String resultado = frase.replace(caracter1, caracter2);
 
-System.out.println(resultado);
+            System.out.println(resultado);
 
-
-} catch (Exception e) {
-System.out.println("Ocurrió un error " + e.getMessage());
-}
-}
+        } catch (Exception e) {
+            System.out.println("Ocurrió un error " + e.getMessage());
+        }
+    }
 }
